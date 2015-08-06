5aaea4d0cb8098e1c0cb3d810f44eeb6c28c708c
shu223 <shuichi0526@gmail.com>
2015-08-06 21:36:20 +0900

    first commit

---
+++ TokyoOlympicEmblem/TOEView.swift
@@ -0,0 +1,79 @@
+//
+//  TOEView.swift
+//  TokyoOlympicEmblem
+//
+//  Created by Shuichi Tsutsumi on 2015/08/06.
+//  Copyright (c) 2015年 Shuichi Tsutsumi. All rights reserved.
+//
+
+import UIKit
+
+class TOEView: UIView {
+
+    var l: CGFloat!
+    var r: CGFloat!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        l = frame.size.width / 3
+        r = sqrt(1.5 * 1.5 + 0.5 * 0.5) * l
+    }
+    
+    override func drawRect(rect: CGRect) {
+        
+        self.drawYellowArc()
+        self.drawBlackRect()
+        self.drawRedOval()
+        self.drawGreyArc()
+    }
+
+    
+    // =========================================================================
+    // MARK: - Private
+    
+    private func drawYellowArc() {
+        let path = UIBezierPath(
+            arcCenter: CGPointMake(l * 1.5, l * 1.5),
+            radius: r,
+            startAngle: CGFloat(-M_PI_2),
+            endAngle: CGFloat(-M_PI),
+            clockwise: false)
+        path.addLineToPoint(CGPointZero)
+        
+        UIColor(red: 1.0, green: 0.84, blue: 0.0, alpha: 1.0).setFill()
+        path.fill()
+    }
+
+    private func drawBlackRect() {
+        
+        let rect = CGRectMake(l, 0, l, l * 3)
+        let path = UIBezierPath(rect: rect)
+        
+        UIColor.blackColor().setFill()
+        path.fill()
+    }
+
+    private func drawRedOval() {
+
+        let rect = CGRectMake(l * 2, 0, l, l)
+        let path = UIBezierPath(ovalInRect: rect)
+        
+        UIColor.redColor().setFill()
+        path.fill()
+    }
+    
+    private func drawGreyArc() {
+        
+        let path = UIBezierPath(
+            arcCenter: CGPointMake(l * 1.5, l * 1.5),
+            radius: r,
+            startAngle: 0,
+            endAngle: CGFloat(M_PI_2),
+            clockwise: true)
+        path.addLineToPoint(CGPointMake(l * 3, l * 3))
+        
+        UIColor(white: 0.5, alpha: 1.0).setFill()
+        path.fill()
+    }
+}
---
+++ TokyoOlympicEmblem/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TokyoOlympicEmblem
+//
+//  Created by Shuichi Tsutsumi on 2015/08/06.
+//  Copyright (c) 2015年 Shuichi Tsutsumi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
